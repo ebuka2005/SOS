f58501e8456cd551c7d78f9460e89afd87f96f0c
ebuka2005 <chiebukaonyejesi@gmail.com>
2024-10-12 14:30:58 -0400

    Updated Firebase configuration

--- src/app/firebase.jsx
+++ src/app/firebase.jsx
@@ -2,14 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
+// Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyBF78Ou7aS7mDriuMoW4ydKIfG1O3gNacE",
-  authDomain: "sos-ett.firebaseapp.com",
-  projectId: "sos-ett",
-  storageBucket: "sos-ett.appspot.com",
-  messagingSenderId: "86838142122",
-  appId: "1:86838142122:web:8bcb1910cb460d673a5cd3",
-  measurementId: "G-4QZWC20PZS"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
@@ -18,12 +19,14 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication
 const auth = getAuth(app);
 
+// Sign-up function
 export const signUp = (email, password) => {
-    return createUserWithEmailAndPassword(auth, email, password);
-  };
-  
-  export const login = (email, password) => {
-    return signInWithEmailAndPassword(auth, email, password);
-  };
-  
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+// Login function
+export const login = (email, password) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
 export { auth };
